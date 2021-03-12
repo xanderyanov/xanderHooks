37b2b49f3f4077034df1356eaadb1f3e994d2d55
xanderyanov <xanderyanov@gmail.com>
2021-03-12 14:49:14 +0400

    var target = $(e.target);

--- hooks.js
+++ hooks.js
@@ -1,1 +1,15 @@
 console.log('Hello, World!');
+
+
+
+// Клик по элементу внутри l1tabsTabs__overlay запрашивает внутренние элементы
+$(".l1tabsTabs__overlay").on("click", overlayClickedToClose);
+function overlayClickedToClose(e) {
+  var target = $(e.target);
+  if (target.is('.l1tabsTabs__overlayInner, .l1tabs__close, .l1tabs__close *')) {
+      $("body").removeClass("stop");
+      $(".l1tabsTabs__overlay").hide();
+      $(".l1tabsBrands__itemBoxOuter").hide();
+      e.stopPropagation();
+  }
+}
